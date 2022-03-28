0e0ee759edf58c369479d32d2099a4899303b0cb
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-28 08:19:59 -0400

    getting section 2 started

--- src/main/java/models/Comics.java
+++ src/main/java/models/Comics.java
@@ -1,4 +1,10 @@
 package models;
 
 public class Comics {
+    private int id;
+    private String name;
+    private String publisher;
+    private int qty;
+    private double grade;
+    private float price;
 }
