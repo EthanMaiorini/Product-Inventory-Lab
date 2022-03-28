9cae2194bcd4fa0f8f323b309a4090c3b995a491
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-28 08:28:02 -0400

    getting section 2 started

--- src/main/java/models/Tshirts.java
+++ src/main/java/models/Tshirts.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.awt.*;
+
 public class Tshirts {
+
+    private int id;
+    private String name;
+    private String brand;
+    private int size;
+    private Color color;
+    private int qty;
+    private float price;
 }
