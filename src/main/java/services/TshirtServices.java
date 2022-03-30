19efb54264296587c5feb20b1e41c9908fd0f482
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-29 22:04:19 -0400

    Part2 complete

--- src/main/java/services/TshirtServices.java
+++ src/main/java/services/TshirtServices.java
@@ -21,4 +21,44 @@ public class TshirtServices {
         // (4)
         return createdTshirt;
     }
+
+    public Tshirts findTshirts(int id) {
+        // should take an int and return an object with that id, if exists
+        Tshirts comic;
+        for(Tshirts c: inventory){
+            if(c.getId() == id)
+                return c;
+        }
+        return null;
+    }
+
+    public int getId(Tshirts name){
+        int x = inventory.indexOf(name);
+        Tshirts comic = inventory.get(x);
+        return comic.getId();
+    }
+
+    //read all
+    public Tshirts[] findAllTshirts() {
+        // should return a basic array copy of the ArrayList
+        Tshirts[] result = new Tshirts[inventory.size()];
+        int x =0;
+        for(Tshirts c : inventory) {
+            result[x] = c;
+            x++;
+        }
+        return result;
+    }
+
+    //delete
+    public boolean delete(int id) {
+        // should remove the object with this id from the ArrayList if exits and return true.
+        // Otherwise return false
+        Tshirts comic = findTshirts(id);
+        if(comic != null) {
+            inventory.remove(comic);
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/resources/TshirtsServicesTest.java
+++ src/test/java/resources/TshirtsServicesTest.java
@@ -46,4 +46,102 @@ public class TshirtsServicesTest {
         Assertions.assertEquals(expectedPrice, actualPrice);
 
     }
+
+    @Test
+    public void findComicsTest(){
+        // (1)
+        String expectedName = "Amazing Spider-man";
+        String expectedBrand = "Marvel";
+        int expectedSize = 10;
+        int expectedQty = 10;
+        Color expectedColor = Color.BLACK;
+        float expectedPrice = 80.00f;
+
+        // (2)
+        TshirtServices tshirtsService = new TshirtServices();
+        Tshirts testTshirts = TshirtServices.create(expectedName, expectedBrand,
+                expectedSize, expectedColor, expectedQty, expectedPrice);
+
+        // (3)
+        Tshirts actualTshirts = tshirtsService.findTshirts(testTshirts.getId());
+        int actualId = actualTshirts.getId();
+        String actualName = actualTshirts.getName();
+        String actualBrand = actualTshirts.getBrand();
+        Color actualColor = actualTshirts.getColor();
+        int actualSize = actualTshirts.getSize();
+        int actualQty = actualTshirts.getQty();
+        float actualPrice = actualTshirts.getPrice();
+
+
+        // (4)
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedBrand, actualBrand);
+        Assertions.assertEquals(expectedSize, actualSize);
+        Assertions.assertEquals(expectedColor, actualColor);
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+
+    @Test
+    public void findAllComicsTest(){
+        // (1)
+        String expectedName = "Amazing Spider-man";
+        String expectedBrand = "Marvel";
+        int expectedSize = 10;
+        int expectedQty = 10;
+        Color expectedColor = Color.BLACK;
+        float expectedPrice = 80.00f;
+
+        // (2)
+        TshirtServices tshirtsService = new TshirtServices();
+        Tshirts testTshirts = TshirtServices.create(expectedName, expectedBrand,
+                expectedSize, expectedColor, expectedQty, expectedPrice);
+
+        // (3)
+        Tshirts[] actualTshirts = tshirtsService.findAllTshirts();
+        int actualId = actualTshirts[0].getId();
+        String actualName = actualTshirts[0].getName();
+        String actualBrand = actualTshirts[0].getBrand();
+        Color actualColor = actualTshirts[0].getColor();
+        int actualSize = actualTshirts[0].getSize();
+        int actualQty = actualTshirts[0].getQty();
+        float actualPrice = actualTshirts[0].getPrice();
+
+
+        // (4)
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedBrand, actualBrand);
+        Assertions.assertEquals(expectedSize, actualSize);
+        Assertions.assertEquals(expectedColor, actualColor);
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+    }
+
+    @Test
+    public void deleteTest(){
+        String expectedName = "Amazing Spider-man";
+        String expectedBrand = "Marvel";
+        int expectedSize = 10;
+        int expectedQty = 10;
+        Color expectedColor = Color.BLACK;
+        float expectedPrice = 80.00f;
+
+        // (2)
+        TshirtServices tshirtsService = new TshirtServices();
+        Tshirts testTshirts = TshirtServices.create(expectedName, expectedBrand,
+                expectedSize, expectedColor, expectedQty, expectedPrice);
+
+        // (3)
+        int getInt = tshirtsService.getId(testTshirts);
+        Boolean actualTshirt = tshirtsService.delete(getInt);
+
+
+
+        // (4)
+        Assertions.assertTrue(actualTshirt);
+        //Assertions.assertEquals(true,);
+    }
+
 }
